917b86c419d9ef1b87f8f771a960210f0f698644
dianagamez <diana.gamez.fon@gmail.com>
2014-09-23 10:46:11 -0500

    test failed

--- NumberSystemConverter/RomanNumeralConverter.cs
+++ NumberSystemConverter/RomanNumeralConverter.cs
@@ -9,13 +9,7 @@ namespace NumberSystemConverter
     {
         public string ConvertRomanNumeral(int p)
         {
-            if (p < 1 || p > 3000)
-            {
-                throw new IndexOutOfRangeException(
-                  "The number supplied is out of the expected range (1 - 3000).");
-            }
-
-            return string.Empty;
+            throw new NotImplementedException();
         }
     }
 }
