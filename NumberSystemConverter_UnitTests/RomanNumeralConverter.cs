e7e9541826cc730fef4ddf299832db725c5a83b3
dianagamez <diana.gamez.fon@gmail.com>
2014-09-23 09:59:47 -0500

    first commit test failed

---
+++ NumberSystemConverter_UnitTests/RomanNumeralConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NumberSystemConverter_UnitTests
+{
+    class RomanNumeralConverter
+    {
+        internal void ConvertRomanNumeral(int p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NumberSystemConverter_UnitTests/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumberSystemConverter_UnitTests;
+
+namespace NumberSystemConverter_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void Number_Greater_Than_ThreeThousand_Throws_IndexOutOfRangeException_TestMethod()
+        {
+            var converter = new RomanNumeralConverter();
+            converter.ConvertRomanNumeral(3001);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void Number_Less_Than_One_Throws_IndexOutOfRangeException_TestMethod()
+        {
+            var converter = new RomanNumeralConverter();
+            converter.ConvertRomanNumeral(-1);
+        }
+
+    }
+}
